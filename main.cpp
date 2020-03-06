24454fe5dd527084724dff9b5d4d2539c57db834
srchd <sipirichi@gmail.com>
2020-03-06 10:55:47 +0100

    kurva elet

--- main.cpp
+++ main.cpp
@@ -6,7 +6,19 @@ using namespace std;
 bool tallest_mexican(const string& file_name, Cactus& elem);
 
 int main(){
-    std::cout << "Hello World!\n";
+    try {
+        Cactus elem;
+        if (tallest_mexican("input.txt", elem)) {
+            cout << "A legmagasabb mexikoi kaktusz: " << elem << endl;
+        }
+        else
+            cout << "noncs" << endl;
+    }
+    catch (in_file::Error exc) {
+        if (exc == in_file::OPEN_ERROR)
+            cout << "Nincs ilye fajl, fonok, hozzad letre izibe" << endl; exit(1);
+    }
+    return 0;
 }
 
 
